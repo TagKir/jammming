47416fdaeab12a8b941ce1309055eda2670eac01
TagKir <tagirkiky@gmail.com>
2024-01-02 01:22:01 +0300

    changed 6 files

--- src/components/search_results/SearchResults.js
+++ src/components/search_results/SearchResults.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./SearchResults.css";
 
 export default function SearchResults() {
   return <button className="search_song">Search Song</button>;
--- src/components/tracklist/Tracklist.js
+++ src/components/tracklist/Tracklist.js
@@ -6,14 +6,22 @@ export default function Tracklist() {
   return (
     <div className="all">
       <div className="tracks">
-        <h2 className="result">Result</h2>
+        <h2>Result</h2>
         <ul>
           {cards.map((card) => (
             <li>{card}</li>
           ))}
         </ul>
       </div>
-      <div className="chosen"></div>
+      <div className="chosen">
+        <input className="playlist_name_input" />
+        <ul>
+          {cards.map((card) => (
+            <li>{card}</li>
+          ))}
+        </ul>
+        <button className="save">SAVE TO SPOTIFY</button>
+      </div>
     </div>
   );
 }
