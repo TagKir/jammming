6cb6774083b0de51096236db32730da5670bcf27
TagKir <tagirkiky@gmail.com>
2024-01-02 16:04:28 +0300

    modified Tracklist.css and Tracklist.js in tracklist

--- src/components/tracklist/Tracklist.js
+++ src/components/tracklist/Tracklist.js
@@ -2,24 +2,48 @@ import "./Tracklist.css";
 import React, { useState } from "react";
 
 export default function Tracklist() {
-  let cards = ["Hell of the Book"];
+  let cards = [
+    {
+      name: "Hell of the Book",
+      artist: "Hell",
+      album: "None",
+      id: 1,
+    },
+    {
+      name: "Hello",
+      artist: "BBB",
+      album: "WLR",
+      id: 1,
+    },
+  ];
+
   return (
     <div className="all">
       <div className="tracks">
         <h2>Result</h2>
-        <ul>
-          {cards.map((card) => (
-            <li>{card}</li>
-          ))}
-        </ul>
+        {cards.map((card) => {
+          return (
+            <ul>
+              <li className="name">{card.name}</li>
+              <li className="artist_album">
+                {card.artist} | {card.album}
+              </li>
+            </ul>
+          );
+        })}
       </div>
       <div className="chosen">
         <input className="playlist_name_input" />
-        <ul>
-          {cards.map((card) => (
-            <li>{card}</li>
-          ))}
-        </ul>
+        {cards.map((card) => {
+          return (
+            <ul>
+              <li className="name">{card.name}</li>
+              <li className="artist_album">
+                {card.artist} | {card.album}
+              </li>
+            </ul>
+          );
+        })}
         <button className="save">SAVE TO SPOTIFY</button>
       </div>
     </div>
